3ed09a7daa48bb30377d46a62f0dc604b884d854
leehan <yye572@gmail.com>
2021-04-22 23:10:16 +0900

    first commit

---
+++ src/main/java/com/taskagaile/TaskAgaileApplication.java
@@ -0,0 +1,13 @@
+package com.taskagaile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskAgaileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskAgaileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taskagaile/TaskAgaileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskagaile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskAgaileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
